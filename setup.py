6a3aa6254af1d820727ea15a9c783063edfc2166
Jiri Raja <jiri.raja@gmail.com>
2022-01-04 20:28:30 +0100

    updated license, requirements, version

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-setup(name='cryton_worker',
-      version='2021.4.1',
-      description='Cryton Worker - Attack scenario orchestrator',
-      url='https://gitlab.ics.muni.cz/beast-public/cryton/cryton-worker',
-      author='Ivo Nutar, Jiri Raja, Andrej Tomci, Milan Bohacek',
-      author_email='nutar@ics.muni.cz, 469429@mail.muni.cz, 469192@mail.muni.cz, 244656@mail.muni.cz',
+setup(name="cryton_worker",
+      version="2022.1.1",
+      description="Cryton Worker - Attack scenario orchestrator",
+      url="https://gitlab.ics.muni.cz/beast-public/cryton/cryton-worker",
+      author="Ivo Nutar, Jiri Raja, Andrej Tomci, Milan Bohacek",
+      author_email="nutar@ics.muni.cz, 469429@mail.muni.cz, 469192@mail.muni.cz, 244656@mail.muni.cz",
       packages=find_packages(),
-      python_requires='>=3.8',
+      python_requires=">=3.8",
       install_requires=requirements,
       entry_points={
-          'console_scripts': [
-              'cryton-worker=cryton_worker.cli:cli'
+          "console_scripts": [
+              "cryton-worker=cryton_worker.cli:cli"
           ]
       },
       zip_safe=False
